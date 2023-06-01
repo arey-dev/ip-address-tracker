81539834780712036505167c18f913f5e5fac3a3
R.A Borja <raborja.dev@gmail.com>
2023-06-01 16:15:32 +0800

    declare props type for Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,4 +1,8 @@
-export function Container(props) {
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export function Container(props: ContainerProps) {
   return (
     <div className="max-w-6xl w-full mx-auto px-6 py-8 max-h-[18.75rem] sm:max-h-[17.5rem]">
       {props.children}
